a88dcfc2a5bb96f19ae95cf7cfd172b033067807
SharonPeled <ssharon95@gmail.com>
2025-07-23 14:53:08 +0300

    fixing padding transform

--- src/objects/PaddingTransform.py
+++ src/objects/PaddingTransform.py
@@ -3,21 +3,19 @@ import torch.nn.functional as F
 
 
 class PaddingTransform:
-    def __init__(self, objective, type_, value, patch_len):
+    def __init__(self, objective, type_, patch_len):
         """
-        Initializes the padding transform with the provided type and value.
+        Initializes the padding transform.
         
         Args:
             objective (str): The objective of the padding.
             type_ (str): The type of padding to apply.
-            value (float): The value to use for padding.
             patch_len (int): The length of the patch to pad.
         """
         if objective != "patching":
             raise NotImplementedError("Only patching padding is implemented yet")
-        if type_ != "constant":
-            raise NotImplementedError("Only constant padding is implemented yet")
-        self.value = value
+        if type_ != "nan_padding":
+            raise NotImplementedError("Only nan padding is implemented yet")
         self.patch_len = patch_len
 
     def __call__(self, sample):
@@ -36,7 +34,7 @@ class PaddingTransform:
         if T % self.patch_len != 0:
             # Calculate padding needed at start
             pad_size = self.patch_len - (T % self.patch_len)
-            ts = F.pad(ts, (0, 0, pad_size, 0), mode='constant', value=self.value)
+            ts = F.pad(ts, (0, 0, pad_size, 0), mode='constant', value=torch.nan)
             nan_mask = F.pad(nan_mask, (0, 0, pad_size, 0), mode='constant', value=True)
         
         sample['ts'] = ts
